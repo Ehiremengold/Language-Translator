07f3e53a41a1984b8ba933eaafdad1941e55ae2d
Ibhafidon ehiremen francis <Ehiremengold@gmail.com>
2019-03-29 23:50:52 -0700

    Update langtrans.py

--- langtrans.py
+++ langtrans.py
@@ -1,6 +1,7 @@
 def translator(phrase):
  translation = ""
  for letter in phrase:
+  #In the program, I made the letter "j" "J" a substitute for a vowel in any word inputed 
         if letter in "AEIOU":
             translation += "J"
         elif letter in "aeiou":
